ff8f9c59aa5dd95bd0a4f5211e09cc4dd14c3805
Pavan Podila <pavan.podila@gmail.com>
2018-03-17 19:17:32 +0530

    validations

--- src/examples/02-form.tsx
+++ src/examples/02-form.tsx
@@ -7,10 +7,25 @@ import { action, observable, reaction } from 'mobx';
 import { observer } from 'mobx-react';
 import FormItem from 'antd/lib/form/FormItem';
 import Button from 'antd/lib/button/button';
+import * as validator from 'validate.js';
 
 class FormData {
     @observable username = '';
     @observable password = '';
+
+    @observable.ref validation = null;
+
+    static rules = {
+        username: {
+            presence: { allowEmpty: false },
+        },
+        password: {
+            presence: { allowEmpty: false },
+            length: {
+                minimum: 3,
+            },
+        },
+    };
 
     constructor() {
         reaction(
@@ -18,7 +33,7 @@ class FormData {
                 const { username, password } = this;
                 return { username, password };
             },
-            () => {},
+            () => this.validateFields(),
         );
     }
 
@@ -29,17 +44,38 @@ class FormData {
 
     @action
     submit() {}
+
+    private validateFields() {
+        this.validation = validator.validate(this, FormData.rules);
+    }
 }
 const formData = new FormData();
 
 @observer
 export class Form extends React.Component {
     render() {
-        const { username, password } = formData;
+        const { username, password, validation } = formData;
         return (
             <Row>
                 <Col span={12} offset={6}>
-                    <FormItem label={'Username'}>
+                    <FormItem
+                        label={'Username'}
+                        validateStatus={
+                            validation && validation['username']
+                                ? 'error'
+                                : undefined
+                        }
+                        help={
+                            validation && validation['username']
+                                ? validation['username'][0]
+                                : undefined
+                        }
+                        hasFeedback={
+                            validation && validation['username']
+                                ? true
+                                : undefined
+                        }
+                    >
                         <Input value={username} onChange={this.setUsername} />
                     </FormItem>
 
