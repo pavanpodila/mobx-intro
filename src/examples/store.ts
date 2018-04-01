a42d02d80c552c1dc9384a5b57f57c37b38eebed
pavanpodila <pavan.podila@gmail.com>
2018-04-01 09:29:43 +0530

    removing files

--- src/examples/store.ts
+++
@@ -1,8 +0,0 @@
-import { Clock } from './01-clock';
-import { Form } from './02-form';
-
-export const examples = [
-    { label: 'Clock', path: '/examples/01', component: Clock },
-    { label: 'Form Handling', path: '/examples/02', component: Form },
-];
-
